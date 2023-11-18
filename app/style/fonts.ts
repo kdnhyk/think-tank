6df635b494b8de5ce08e1f7e977ee19cbe0c7bb7
kdnhyk <kdnhyk@gmail.com>
2023-11-18 16:56:31 +0900

    [feat] 폰트 설정

---
+++ app/style/fonts.ts
@@ -0,0 +1,19 @@
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+
+const inter = Inter({
+  display: "swap",
+  variable: "--inter",
+});
+const hiragino_sans = localFont({
+  src: [
+    {
+      path: "../../public/font/HiraginoKakuGothicProW3.otf",
+    },
+  ],
+  display: "swap",
+  weight: "300",
+  variable: "--hiragino_sans",
+});
+
+export { inter, hiragino_sans };
