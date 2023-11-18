3ca8cf11c81ce36218fa90b75664050a9979a54b
kdnhyk <kdnhyk@gmail.com>
2023-11-18 19:24:31 +0900

    [mod] 슬롯 @auth -> @modal 이름 변경

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import Navigation from "@/(components)/Navigation";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 export default function Layout({
   children,
-  auth,
+  modal,
 }: {
   children: React.ReactNode;
-  auth: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   const [queryClient] = useState(
     new QueryClient({
@@ -23,13 +24,14 @@ export default function Layout({
     })
   );
 
-  const isAuth = false;
-
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <div className="h-full bg-black">{children}</div>
-        {!isAuth && auth}
+        <main className="bg-black full">{children}</main>
+
+        {modal}
+
+        <Navigation />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </>
