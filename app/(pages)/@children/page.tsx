2be211fafbd94c4ae94ba9e3b8805fda4f9b6b26
kdnhyk <kdnhyk@gmail.com>
2023-11-18 19:26:59 +0900

    [feat] @children 기본 페이지

--- app/(pages)/@children/page.tsx
+++ app/(pages)/@children/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 const MainPage = () => {
+  const router = useRouter();
+
   return (
-    <main>
-      <div>hi</div>
+    <main className="p-4 flex-center">
+      <div
+        className="w-20 h-20 bg-red-400 cursor-pointer rounded-full flex-center"
+        onClick={() => router.push("/star/2")}
+      >
+        <p>예시 별</p>
+      </div>
+      <div
+        className="w-20 h-20 bg-red-400 cursor-pointer rounded-full flex-center"
+        onClick={() => router.push("/star/3")}
+      >
+        <p>예시 별</p>
+      </div>
     </main>
   );
 };
