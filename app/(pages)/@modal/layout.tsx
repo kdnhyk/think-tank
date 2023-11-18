411c7d0e5109ed43eb57d57238b8e4085f267ea0
kdnhyk <kdnhyk@gmail.com>
2023-11-18 20:45:26 +0900

    [mod] 로그인시 user 전역 설정

--- app/(pages)/@modal/layout.tsx
+++ app/(pages)/@modal/layout.tsx
@@ -1,18 +1,31 @@
 "use client";
 
 import useUser from "@/hook/use-user";
+import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const ModalLayout = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useUser();
+  const { user, setUser } = useUser();
   const router = useRouter();
-
   const pathname = usePathname();
+  const { data: session, status } = useSession();
 
   useEffect(() => {
-    if (!user) router.push("/auth");
+    if (status !== "loading" && !user) router.push("/auth");
   }, [pathname, user]);
+
+  useEffect(() => {
+    console.log(session);
+
+    if (status === "authenticated" && !user && session.user?.email) {
+      // post
+      setUser({
+        id: 0,
+        email: session.user?.email,
+      });
+    }
+  }, [status, user]);
 
   return <>{children}</>;
 };
--- app/type/user.ts
+++ app/type/user.ts
@@ -1,5 +1,5 @@
 export interface UserType {
   id: number;
-  name: string;
+  name?: string;
   email: string;
 }
